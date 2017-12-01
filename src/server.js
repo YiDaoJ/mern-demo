5db8283de1c8733a2a2e816f5cceea970a55f3ac
Xiao <jcxsusan@gmail.com>
2017-12-01 15:14:15 +0100

    create api, render react in server

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router';
+import apiRouter from './api';
+import config from '../config';
+import App from './App';
+import Html from './Html';
+
+const app = express();
+
+// app.use('/assets', express.static('assets'));  // ??
+
+app.get('/', (req, res) => {
+  const appString = renderToString(<StaticRouter><App /></StaticRouter>);
+  res.send(Html({body: appString, title: 'i18nTest'}));
+});
+
+app.use('/api', apiRouter);
+
+app.listen(config.port, () => {
+  console.info('Express listening on port ', config.port);
+});
